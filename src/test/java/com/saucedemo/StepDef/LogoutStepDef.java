c139f15b037ddb988d359953c172b78bc272cc02
Azriani Mustakim <azriani.m@gmail.com>
2023-12-28 20:12:55 +0800

    update code

--- src/test/java/com/saucedemo/StepDef/LogoutStepDef.java
+++ src/test/java/com/saucedemo/StepDef/LogoutStepDef.java
@@ -1,58 +1,32 @@
 package com.saucedemo.StepDef;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
+import com.saucedemo.BaseTest;
+import com.saucedemo.pages.HomePage;
+import com.saucedemo.pages.LogoutPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import java.time.Duration;
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class LogoutStepDef {
-    WebDriver driver;
-    @Before
-    public void beforeAll() {
-        driver = WebDriverManager.chromedriver().create();
-        driver.manage().window().maximize();
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-    }
-    @After
-    public void afterAll() {
-        driver.close();
-    }
-
+public class LogoutStepDef extends BaseTest {
+    LogoutPage logoutPage;
     @Given("User is on the homepage")
     public void userIsOnTheHomepage() {
-        By homepageProducts = By.xpath("//*[@id=\"header_container\"]/div[2]/span");
-        WebElement productElement = driver.findElement(homepageProducts);
-        assertTrue(productElement.isDisplayed());
-        assertEquals("Products", productElement.getText());
+        logoutPage = new LogoutPage(driver);
+        logoutPage.userInHomepage();
     }
+    @When("User click on the BurgerMenu")
+    public void userClickOnTheBurgerMenu() {
+        logoutPage.clickMenu();
 
-    @When("User click on the Burgermenu")
-    public void userClickOnTheBurgermenu() {
-        driver.findElement(By.id("react-burger-menu-btn")).click();
     }
-
     @And("Click on the Logout menu")
     public void clickOnTheLogoutMenu() {
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.findElement(By.id("logout_sidebar_link")).click();
-    }
+        logoutPage.clickLogout();
 
+    }
     @Then("The page is redirected to the login page")
     public void thePageIsRedirectedToTheLoginPage() {
-        WebElement usernameField = driver.findElement(By.id("user-name"));
-        assertTrue(usernameField.isDisplayed());
-        System.out.println("Logout Success");
+
     }
 }
---
+++ src/test/java/com/saucedemo/pages/LogoutPage.java
@@ -0,0 +1,2 @@
+package com.saucedemo.pages;public class LogoutPage {
+}
